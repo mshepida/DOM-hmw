714a446288fd979cfd262e9255084ac3c1e4ad21
Mykola Shepida <mshepida@gmail.com>
2017-12-12 19:51:13 +0200

    fixed date thing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ form.setAttribute('name',"login");
 form.setAttribute('action',"google.com");
 form.setAttribute('onsubmit',"return validate()");
 
+
 var inputAge = document.createElement("input"); //Age, text
 inputAge.setAttribute('type',"text");
 inputAge.setAttribute('name',"age");
@@ -12,12 +13,12 @@ inputAge.setAttribute('placeholder',"Input Your Age");
 var inputUserName = document.createElement("input"); //User name, text
 inputUserName.setAttribute('type',"text");
 inputUserName.setAttribute('name',"username");
-inputUserName.setAttribute('placeholder',"Input your name");
+inputUserName.setAttribute('placeholder',"Your name:(user_[name])");
 
 var inputDate = document.createElement("input"); //date, text
 inputDate.setAttribute('type',"text");
 inputDate.setAttribute('name',"date");
-inputDate.setAttribute('placeholder',"Input Date");
+inputDate.setAttribute('placeholder',"Input Date (dd/mm/yyyy)");
 
 var submitButton = document.createElement("input"); // Submit button
 submitButton.setAttribute('type',"submit");
@@ -32,13 +33,21 @@ document.getElementsByTagName('body')[0].appendChild(form);
 
 function validate() {
 	var usernameValid = /^user_+[0-9a-z@]/i;
-	var dateValid = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+	//var dateValid = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+	var date = new Date();
+	var currDate = date.getDate();
+	var currMonth = date.getMonth() + 1;
+	var currYear = date.getFullYear();
+	var fullDate = currDate + "/" + currMonth + "/" + currYear;
 
-	if(!inputAge.value || !isFinite(inputAge.value) || inputAge.value <= 0 ){
-		alert("Your age is invalid");
 
-		return false;
-	}
+	if (! /^[0-9]+$/.test(inputAge.value)) {
+        alert("Your age is invalid");
+
+
+        return false;
+}
+
 
 	if(usernameValid.test(inputUserName.value) == false){
 		alert("Your name is invalid");
@@ -46,7 +55,7 @@ function validate() {
 		return false;
 	}
 
-	if (dateValid.test(inputDate.value) == false) {
+	if (inputDate.value !== fullDate) {
 		alert("Your date is invalid");
 
 		return false;
