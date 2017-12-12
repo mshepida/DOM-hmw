046abbb10ad918d4a818b68367271c23f198edfe
Mykola Shepida <mshepida@gmail.com>
2017-12-12 20:09:52 +0200

    fixed date work

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.getElementsByTagName('body')[0].appendChild(form);
 
 function validate() {
 	var usernameValid = /^user_+[0-9a-z@]/i;
-	//var dateValid = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+
 	var date = new Date();
 	var currDate = date.getDate();
 	if(currDate<10) currDate = "0" + currDate;
